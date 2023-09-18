df34aec458aff548fe5cae58532be118213e6b4f
neville777 <neville.james@student.moringaschool.com>
2023-09-18 08:49:22 +0300

    objects-lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution in this file!
+// Initialize the employee object
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main Street",
+  };
+  
+  // Function to update employee with a new key-value pair (non-destructive)
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+   
+    return { ...employee, [key]: value };
+  }
+  
+  // Function to update employee with a new key-value pair (destructive)
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   
+    employee[key] = value;
+    return employee; 
+  }
+  
+  // Function to delete a key-value pair from the employee object (non-destructive)
+  function deleteFromEmployeeByKey(employee, key) {
+    
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee; 
+  }
+  
+  // Function to delete a key-value pair from the employee object
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; 
+    return employee; 
+  }
+  
+  
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "age", 30);
+  console.log(updatedEmployee);
+  
+  const destructivelyUpdatedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "city", "New York");
+  console.log(destructivelyUpdatedEmployee);
+  
+  const employeeWithoutStreetAddress = deleteFromEmployeeByKey(employee, "streetAddress");
+  console.log(employeeWithoutStreetAddress);
+  
+  const destructivelyUpdatedEmployeeWithoutName = destructivelyDeleteFromEmployeeByKey(employee, "name");
+  console.log(destructivelyUpdatedEmployeeWithoutName);
+  
